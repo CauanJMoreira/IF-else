665d6924d4159786f20b980499269bb2009e0588
Cauan Josias Moreira <168428906+CauanJMoreira@users.noreply.github.com>
2024-09-02 12:27:00 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new
+      Scanner(System.in);
+    System.out.println("login");
+
+    String login = sc.next();
+  int senhaAdmin = 1234;
+    int login1 = 1;
+    
+if (login == login1) {System.out.println("digite sua senha");
+           int senha = sc.nextInt();
+               int (senha = senhaAdmin) {
+            System.out.println("Acesso permitido");
+               } else {
+  System.out.println ("Acesso negado");
+               }         
+} else {System.out.println("Login inválido");}
+  
+    sc.close();
+  }
+
+}
